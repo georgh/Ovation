91a8236e650563e4eaa8a28c05a91ebc5cb7433b
Team Darko <c.bauer@insiders-technologies.de>
2017-09-19 20:06:22 +0200

    Refactor

--- demos/appointments/logic.py
+++ demos/appointments/logic.py
@@ -1,5 +1,17 @@
+from enum import Enum
 
+class SessionState(Enum):
+    DONE = 0
+    CONTINUE = 1 
+    
+
+class ResultObject:
+    def __init__(self, text, session_state):
+        self.text = text
+        self.session_state = session_state
+
+    
 def response(text):
     print ("Interprete ", text)
-    return "You can come now"
+    return ResultObject("You can come now", SessionState.DONE)
     
