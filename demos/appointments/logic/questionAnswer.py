73ca44495dc35f8f938cd0314421cf3f42978915
Michele Alberti <albertimichele91@hotmail.com>
2017-09-19 21:44:06 +0200

    Merge branch 'feature/appointment' of https://github.com/georgh/Ovation into feature/appointment
    # Conflicts:
    #	servers/database.py
    #	servers/handleDatabase.py

---
+++ demos/appointments/logic/questionAnswer.py
@@ -0,0 +1,9 @@
+"""
+This class implements the logic of the QA for the bot aka it chooses
+a meaningful answer for the user based on the current slots available
+and the last questions asked (so that we do not repeat them)
+"""
+
+
+def nextQuestion():
+    return "BotQuestion"
