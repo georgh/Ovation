01d6d3233e4ae2704cc77718da81cccad06ecc87
Georg Hieronimus <georghieronimus@gmail.com>
2017-09-20 09:23:51 +0200

    adding text switch

--- demos/appointments/appointments.py
+++ demos/appointments/appointments.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-
-from speech import Speech
 from logic import logic
 from understanding import understand
+import argparse
+
+parser = argparse.ArgumentParser()
+#parser.add_argument('l', type=str) #necessary string option
+parser.add_argument('-t', '-text', '--text', action='store_true', default=False, help="Switch to text-only mode")
+args = parser.parse_args() 
+
+if not args.text:
+    from speech import Speech
 
 def listen_loop(io):
     while True:
@@ -16,6 +22,9 @@ def listen_loop(io):
         answer = logic.response(understand(sentence))
         io.say(answer.text)
 
-listen_loop(Speech())
+if args.text:
+    print("TEXT MODE NOT YET IMPLEMENTED")
+else:
+    listen_loop(Speech())
 
 
