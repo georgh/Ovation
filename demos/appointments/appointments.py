bea25374b65d23c496b0de4b855588991697e629
Georg Hieronimus <georghieronimus@gmail.com>
2017-09-21 23:17:36 +0200

    fix reload fuck

--- demos/appointments/appointments.py
+++ demos/appointments/appointments.py
@@ -14,6 +14,7 @@ import os
 def listen_loop(io):
     session = Session(io)
     while io.check():
+        db.loadFromFile()
         session.say("I am the ovation insurance chatbot: how can I help you?")
         session_state = core.SessionState.CONTINUE
         while session_state != core.SessionState.DONE:
