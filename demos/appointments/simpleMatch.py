55d7ef462c16fe2910ba68a80982efffea2c3695
praveen badimala <c.bauer@insiders-technologies.de>
2017-09-21 22:10:44 +0200

    understand morning, evening and afternoon

--- demos/appointments/simpleMatch.py
+++ demos/appointments/simpleMatch.py
@@ -6,27 +6,30 @@ from logic.core import Intent
 INTENT_MAP=[
     (Intent.GOODBYE, [
         "bye", "goodbye", "stop", "end", "farewell"
-    ]),
+    ], []),
     (Intent.AFFIRM, [
         "yes","yep","yeah","indeed","ok", "okay", "great", "accept", "correct", "perfect", "agreed", "agree"
-    ]),
+    ], []),
     (Intent.REJECT, [
         "no", "nope", 
-    ]),
+    ], []),
     (Intent.REPEAT,  [
         "repeat"
-    ]),
+    ], []),
     (Intent.WAIT, [
         "wait"
-    ])
+    ], []),
+    (Intent.POSITIVE, [
+        "morning", "evening", "afternoon"
+    ], ["timespan"])
 ]
 
 def trivial_intent(sentence):
     sentence = sentence.lower()
     sentence =re.sub('[\.! \?]', '', sentence)
     global INTENT_MAP
-    for (intent, words) in INTENT_MAP:
+    for (intent, words, entity) in INTENT_MAP:
         if sentence in words:
-            return intent, True
-    return None, False
+            return intent, entity, True
+    return None, [], False
         
