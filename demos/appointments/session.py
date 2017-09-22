bcaef35fd2f5982c32829a925951c20678e3d6c5
praveen badimala <c.bauer@insiders-technologies.de>
2017-09-22 08:44:56 +0200

    fix repeat twice

--- demos/appointments/session.py
+++ demos/appointments/session.py
@@ -3,17 +3,21 @@ from logic.core import Intent, UserInput, ResultObject, SessionState
 
 class Session:
     def __init__(self, io):
-        self.lastResponse = None
+        self.lastResponse = ""
         self.io = io
 
+    def repeatedResponse(self, response):
+        return "Sure: " + self.lastResponse 
+
     def say(self,response):
-        self.lastResponse = response
+        if response != self.repeatedResponse(self.lastResponse):
+            self.lastResponse = response
         self.io.say(response)
 
     def handleMetaCommand(self, userInput):
         userInput=userInput[0]
         if userInput.intent == Intent.REPEAT and self.lastResponse:
-            return ResultObject("Sure: " + self.lastResponse)
+            return ResultObject(self.repeatedResponse(self.lastResponse))
         elif userInput.intent == Intent.WAIT:
             self.io.say("I'm waiting. Please say {} again, if you want to continue".format("Hello"))
             self.io.check()
